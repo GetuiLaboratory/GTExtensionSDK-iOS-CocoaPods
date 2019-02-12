fe3b5d3aa0659674413bce8cea1f60efe4e1a961
zhaow <zhaow@getui.com>
2019-02-12 11:54:37 +0800

    update：2.2.0

--- GTExtensionSDK.framework/Headers/GeTuiExtSdk.h
+++ GTExtensionSDK.framework/Headers/GeTuiExtSdk.h
@@ -5,6 +5,7 @@
 //  Created by gexin on 16/9/14.
 //  Copyright © 2016年 getui. All rights reserved.
 //
+//  GTExtensionSDK-Version:2.2.0
 
 #import <Foundation/Foundation.h>
 #import <UserNotifications/UserNotifications.h>
@@ -12,16 +13,23 @@
 @interface GeTuiExtSdk : NSObject
 
 /**
- * 当前GeTuiExtSdk版本：2.0.0
+ *  设置语音播报的类(系统的还是其它第三方的)
  */
++ (void)setVoicePlayClass:(id)aClass;
 
 /**
- *  统计APNs到达情况
+ *  只统计APNs到达情况，不下载多媒体资源
  */
-+ (void)handelNotificationServiceRequest:(UNNotificationRequest *)request withComplete:(void (^)(void))completeBlock;
++ (void)handelNotificationServiceRequest:(UNNotificationRequest *)request withComplete:(void (^)(void))completeBlock __attribute((deprecated("建议使用多媒体接口")));
+
 /**
- *  统计APNs到达情况和多媒体推送支持
+ *  统计APNs到达情况和多媒体推送支持, 建议使用该接口
  */
 + (void)handelNotificationServiceRequest:(UNNotificationRequest *)request withAttachmentsComplete:(void (^)(NSArray *attachments, NSArray *errors))completeBlock;
+
+/**
+ * sdk销毁，资源释放
+ */
++ (void)destory;
 
 @end

6636e646390f66c7e1a1c49068cc1e6d64011619
linwanting <linwt@getui.com>
2022-03-16 10:35:42 +0800

    update: 更新2.5.5版本

--- GTExtensionSDK.framework/Headers/GeTuiExtSdk.h
+++ GTExtensionSDK.framework/Headers/GeTuiExtSdk.h
@@ -5,7 +5,7 @@
 //  Created by gexin on 16/9/14.
 //  Copyright © 2016年 getui. All rights reserved.
 //
-//  GTExtensionSDK-Version: 2.5.4
+//  GTExtensionSDK-Version: 2.5.5
 
 #import <Foundation/Foundation.h>
 #import <UserNotifications/UserNotifications.h>
@@ -22,6 +22,12 @@
  *  统计APNs到达情况和多媒体推送支持, 建议使用该接口
  */
 + (void)handelNotificationServiceRequest:(UNNotificationRequest *)request withAttachmentsComplete:(void (^)(NSArray *attachments, NSArray *errors))completeBlock;
+
+
+/**
+ *  设置 App Groups Id (如有使用 iOS Extension SDK，请设置该值)
+ */
++ (void)setApplicationGroupIdentifier:(NSString*)identifier;
 
 /**
  * sdk销毁，资源释放

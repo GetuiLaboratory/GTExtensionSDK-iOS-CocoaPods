7beff09e42e3c354f4c3089cecfe5838af3999e3
linwanting <linwt@getui.com>
2022-04-21 09:53:24 +0800

    [update]:update package.swift

--- Package.swift
+++ Package.swift
@@ -9,7 +9,7 @@ let package = Package(
         // Products define the executables and libraries a package produces, and make them visible to other packages.
         .library(
             name: "GTExtensionSDK",
-            targets: ["GTExtensionSDK2"]),
+            targets: ["GTExtensionSDK"]),
     ],
     dependencies: [
         // Dependencies declare other packages that this package depends on.
@@ -18,12 +18,6 @@ let package = Package(
     targets: [
         // Targets are the basic building blocks of a package. A target can define a module or a test suite.
         // Targets can depend on other targets in this package, and on products in packages this package depends on.
-        .target(
-            name: "GTExtensionSDK",
-            dependencies: []),
-        .testTarget(
-            name: "GTExtensionSDKTests",
-            dependencies: ["GTExtensionSDK"]),
-        .binaryTarget(name: "GTExtensionSDK2", path: "Sources/GTExtensionSDK/GTExtensionSDK.xcframework")
+        .binaryTarget(name: "GTExtensionSDK", path: "Sources/GTExtensionSDK/GTExtensionSDK.xcframework")
     ]
 )

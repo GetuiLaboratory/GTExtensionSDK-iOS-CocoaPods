f94389e1c6d0b55fa1881e6f38e3fb4f95af2883
linwanting <linwt@getui.com>
2022-05-25 16:05:26 +0800

    update: 更新2.5.7版本

--- GTExtensionSDK.framework/Headers/GeTuiExtSdk.h
+++ GTExtensionSDK.framework/Headers/GeTuiExtSdk.h
@@ -5,7 +5,7 @@
 //  Created by gexin on 16/9/14.
 //  Copyright © 2016年 getui. All rights reserved.
 //
-//  GTExtensionSDK-Version: 2.5.6
+//  GTExtensionSDK-Version: 2.5.7
 
 #import <Foundation/Foundation.h>
 #import <UserNotifications/UserNotifications.h>

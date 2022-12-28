19872838af2bb1e90b05b84d0825d00e82db989b
linwanting <linwt@getui.com>
2022-12-28 11:03:46 +0800

    [update]:update package.swift

--- Package.swift
+++ Package.swift
@@ -18,6 +18,6 @@ let package = Package(
     targets: [
         // Targets are the basic building blocks of a package. A target can define a module or a test suite.
         // Targets can depend on other targets in this package, and on products in packages this package depends on.
-        .binaryTarget(name: "GTExtensionSDK", path: "Sources/GTExtensionSDK/GTExtensionSDK.xcframework")
+        .binaryTarget(name: "GTExtensionSDK", path: "GTExtensionSDK.xcframework")
     ]
 )
